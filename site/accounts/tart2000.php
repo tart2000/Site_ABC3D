91b0eb74870f43bd4b6fae731688cf68e9de2573
Arthur Schmitt <tart2000design@gmail.com>
2016-06-16 15:28:51 -0400

    pages ressources ajoutées

---
+++ site/accounts/tart2000.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: tart2000
+email: tart2000design@gmail.com
+password: >
+  $2a$10$iVgYFbVNmNGAptPB4Xx8buqU2hgTf0qvRFFLzH0ykgOywnGZ8dHa2
+language: en
+role: admin
+history:
+  - ressources/liste-de-lecture
